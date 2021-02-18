ccd469e93534c2b2716ae4afc314a4bd8c6c36a7
Volodymyr Kozieiev <vkjr.sp@gmail.com>
2021-02-18 16:07:09 +0200

    Delivered messages have double-check icon in private chats

--- src/status_im/ui/screens/chat/message/message.cljs
+++ src/status_im/ui/screens/chat/message/message.cljs
@@ -43,9 +43,10 @@
                   :align-items                      :flex-end})
     (when (and outgoing justify-timestamp?)
       [icons/icon (case outgoing-status
-                    :sending  :tiny-icons/tiny-pending
-                    :sent     :tiny-icons/tiny-sent
-                    :not-sent :tiny-icons/tiny-warning
+                    :sending   :tiny-icons/tiny-pending
+                    :sent      :tiny-icons/tiny-sent
+                    :not-sent  :tiny-icons/tiny-warning
+                    :delivered :tiny-icons/tiny-delivered
                     :tiny-icons/tiny-pending)
        {:width  16
         :height 12
